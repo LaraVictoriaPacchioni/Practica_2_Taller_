1a302d6f4b6d973b26f7fe633331a626df7521c8
Lara Victoria Pacchioni <laripacchioni07@gmail.com>
2026-03-20 14:11:49 -0300

    segundo ejercicio puntaje

--- questions.py
+++ questions.py
@@ -14,6 +14,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+puntaje = 0
 
 print("¡Bienvenido al Ahorcado!")
 print()
@@ -32,6 +33,7 @@ while attempts > 0:
 
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
+        puntaje += 6
         print("¡Ganaste!")
         break
 
@@ -53,9 +55,14 @@ while attempts > 0:
     else:
         guessed.append(letter)
         attempts -= 1
+        puntaje -= 1
         print("Esa letra no está en la palabra.")
 
     print()
 
 else:
+    puntaje = 0
     print(f"¡Perdiste! La palabra era: {word}")
+
+
+print(f"Tu puntaje final es:{puntaje}")
